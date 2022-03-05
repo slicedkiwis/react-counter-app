c8c29f04c4a36b8fd12d21161fbda8cda4a331a0
slicedkiwis <steevecastor17@gmail.com>
2022-03-04 23:35:39 -0500

    useState hook practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Card from "./components/Card";
+import { useState } from "react";
+import './app.css'
 function App() {
+  const [count,setCount] = useState(0)
+  let add = () =>{
+    setCount(count + 1)
+
+  }
+  let subtract = () =>{
+    setCount(count - 1)
+  }
+  let clear = () =>{
+    setCount(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Card>
+      <h1 className={count > 0 ? "positive": count < 0 ? "negative":null}>{count}</h1>
+      <div className="btns">
+      <button onClick = {add}>+</button>
+      <button onClick = {clear}>clear</button>
+      <button onClick = {subtract}>-</button>
+      </div>
+    </Card>
     </div>
   );
 }
